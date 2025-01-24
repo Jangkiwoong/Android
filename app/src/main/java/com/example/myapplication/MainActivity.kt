1250b03300a1dec1ad9daed42ef05501ded7c888
jangkiwoong <wkdrldnd99@gmail.com>
2025-01-24 17:15:23 +0900

    image 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -22,9 +25,47 @@ class MainActivity : AppCompatActivity() {
             insets
 
         }
-            val image1 = findViewById<ImageView>(R.id.image_1)
-            image1.setOnClickListener {
-                Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_SHORT).show()
-            }
+        val image1 = findViewById<ImageView>(R.id.image_1)
+        val image2 = findViewById<ImageView>(R.id.image_2)
+        val image3 = findViewById<ImageView>(R.id.image_3)
+        val image4 = findViewById<ImageView>(R.id.image_4)
+        val image5 = findViewById<ImageView>(R.id.image_5)
+        val image6 = findViewById<ImageView>(R.id.image_6)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_SHORT).show()//토스트 기능
+
+            val intent = Intent(this, ImageActivity2::class.java)
+            startActivity(intent) //페이지 전환
+        }
+        image2.setOnClickListener {
+            Toast.makeText(this, "2번 클릭 완료", Toast.LENGTH_SHORT).show()//토스트 기능
+
+            val intent = Intent(this, ImageActivity3::class.java)
+            startActivity(intent) //페이지 전환
+        }
+        image3.setOnClickListener {
+            Toast.makeText(this, "3번 클릭 완료", Toast.LENGTH_SHORT).show()//토스트 기능
+
+            val intent = Intent(this, ImageActivity4::class.java)
+            startActivity(intent) //페이지 전환
+        }
+        image4.setOnClickListener {
+            Toast.makeText(this, "4번 클릭 완료", Toast.LENGTH_SHORT).show()//토스트 기능
+
+            val intent = Intent(this, ImageActivity5::class.java)
+            startActivity(intent) //페이지 전환
+        }
+        image5.setOnClickListener {
+            Toast.makeText(this, "5번 클릭 완료", Toast.LENGTH_SHORT).show()//토스트 기능
+
+            val intent = Intent(this, ImageActivity6::class.java)
+            startActivity(intent) //페이지 전환
+        }
+        image6.setOnClickListener {
+            Toast.makeText(this, "6번 클릭 완료", Toast.LENGTH_SHORT).show()//토스트 기능
+
+            val intent = Intent(this, ImageActivity7::class.java)
+            startActivity(intent) //페이지 전환
+        }
     }
 }
