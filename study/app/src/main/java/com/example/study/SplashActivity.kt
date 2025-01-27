13ec2d779ed379facd28b6de0e17aed986d5b4a9
jangkiwoong <wkdrldnd99@gmail.com>
2025-01-27 18:53:25 +0900

    CardView 및 Radius추가

--- study/app/src/main/java/com/example/study/SplashActivity.kt
+++ study/app/src/main/java/com/example/study/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.example.study
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,9 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
