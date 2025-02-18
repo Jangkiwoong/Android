87e4d39cbc217b31a5b9c5608dbac9e1bc7f8368
jangkiwoong <wkdrldnd99@gmail.com>
2025-02-18 20:38:05 +0900

    [Feat] : 명언 리스트

--- Goodwords/app/src/main/java/com/example/goodwords/MainActivity.kt
+++ Goodwords/app/src/main/java/com/example/goodwords/MainActivity.kt
@@ -22,10 +22,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.1")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.2")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.2")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.3")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.4")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.5")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.6")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.7")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.8")
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.showAllSentenceBtn.setOnClickListener {
             val intent = Intent(this, SentenceActivity::class.java)
             startActivity(intent)
         }
+
+        binding.goodWordTextArea.setText(sentenceList.random())
     }
 }
--- Goodwords/app/src/main/java/com/example/goodwords/SentenceActivity.kt
+++ Goodwords/app/src/main/java/com/example/goodwords/SentenceActivity.kt
@@ -1,6 +1,7 @@
 package com.example.goodwords
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,20 @@ class SentenceActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.1")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.2")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.3")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.4")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.5")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.6")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.7")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.8")
+        sentenceList.add("검정화면에 대충 흰글씨 쓰면 명언같다.9")
+
+        val senteceAdapter = ListViewAdapter(sentenceList)
+        val listview = findViewById<ListView>(R.id.sentenceListView)
+
+        listview.adapter = senteceAdapter
     }
 }
